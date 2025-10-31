893bc4a910f98e2f5fe7ce886facd3ad00b4b8f8
Derrick Owusu-Asare <aderrick649@gmail.com>
2025-10-31 19:57:48 +0000

    added a function

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,6 @@
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
 import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
@@ -11,6 +14,9 @@ function App() {
     <>
       <div>
         <WelcomeMessage />
+        <Header />
+        <MainContent />
+        <Footer />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>

8425ff812bc1d2966fefe387097a63dd99f2f652
Derrick Owusu-Asare <aderrick649@gmail.com>
2025-11-06 16:46:22 +0000

    added styling to the trial

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,8 +1,6 @@
 const UserProfile = (props) => {
    return (
-     <div style={{
-      border: '1px solid gray', padding:'10px', margin:'10px'
-     }}>
+     <div style={{ border: '1px solid gray', padding:'10px', margin:'10px' }}>
        <h2 style={{
         color:'blue'
        }}>{props.name}</h2>

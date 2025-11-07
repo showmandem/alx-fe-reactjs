4494038a2b50f8f3a2ac59e9204e8f9012903062
Derrick Owusu-Asare <aderrick649@gmail.com>
2025-11-07 14:59:21 +0000

    changed directory

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,3 +1,8 @@
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import WelcomeMessage from './components/WelcomeMessage'
+import UserProfile from './components/UserProfile'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -9,12 +14,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <WelcomeMessage />
+        <Header />
+        <MainContent />
+        <Footer />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
       </div>
       <h1>Vite + React</h1>
       <div className="card">

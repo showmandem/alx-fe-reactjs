5b9a236cef0a7e2ab77cd96a0c2714f4c6ec3348
Derrick Owusu-Asare <aderrick649@gmail.com>
2025-11-07 15:21:17 +0000

    changed directory

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,18 +21,7 @@ function App() {
         <Footer />
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <Counter />
     </>
   )
 }
---
+++ alx-react-app-new/src/components/Counter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+    </div>
+  )
+}
+
+export default Counter

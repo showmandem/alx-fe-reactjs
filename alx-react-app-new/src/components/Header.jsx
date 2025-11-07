c4699b197741edff2e6711b209128ba662b41bba
Derrick Owusu-Asare <aderrick649@gmail.com>
2025-11-07 15:06:31 +0000

    changed directory

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,6 +1,7 @@
 function Header(){
     return (
-        <header style={{backgroundColor: 'navy', color: 'white', textAlign:'center'}}>
+        <header style={{ backgroundColor: 'navy', color: 'white', textAlign:'center'    
+        }}>
             <h1>My Favorite Cities</h1>
         </header>
    

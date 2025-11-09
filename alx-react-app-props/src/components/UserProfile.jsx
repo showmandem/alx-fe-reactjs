709e4e668cd1c66cf5e9952d241c99c0e06c03f9
Derrick Owusu-Asare <aderrick649@gmail.com>
2025-11-09 16:41:27 +0000

    submission

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,14 +1,17 @@
-import React from "react";
-import UserContext from "./UserContext";
-const UserProfile = (props) => {
+import React, {useContext} from 'react';
+import UserContext from "./UserContext"
+
+const UserProfile = () => {
+
+  const { userData } = useContext(UserContext);
    return (
-     <div style={{border: '1px solid gray', padding:'10px', margin:'10px'}}>
-       <h2 style={{
-        color:'blue'
-       }}>{props.name}</h2>
-       <p>Age: <span style={{fontWeight:'bold'}}>{props.age}</span></p>
-       <p>Bio: {props.bio}</p>
+     <div>
+       <h2>{props.name}</h2>
+       <p>Age: { userData.age}</p>
+       <p>Bio: { userData.bio}</p>
      </div>
    );
  };
+
+
  export default UserProfile

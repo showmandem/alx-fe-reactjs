40b405bd2c485a89c82c0a819fc3786fb409619c
Derrick Owusu-Asare <aderrick649@gmail.com>
2025-12-07 00:26:20 +0000

    styling

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,19 @@
+import React from "react";
+
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto, my-20 rounded-lg shadow-lg">
-      <img src="https://via.placeholder.com/150" alt="User" />
-      <h1>John Doe</h1>
-      <p>Developer at Example Co. Loves to write code and explore new technologies.</p>
+    <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-12 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
+      <img
+        src="https://via.placeholder.com/150"
+        alt="John Doe, developer"
+        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto transition-transform duration-300 ease-in-out hover:scale-110"
+      />
+      <h1 className="text-lg md:text-xl text-blue-800 my-4 text-center transition-colors duration-300 ease-in-out hover:text-blue-500">
+        John Doe
+      </h1>
+      <p className="text-gray-600 text-sm md:text-base text-center">
+        Developer at Example Co. Loves to write code and explore new technologies.
+      </p>
     </div>
   );
 }

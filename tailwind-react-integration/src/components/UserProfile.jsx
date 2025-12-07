300d56960f08542497dc6db1e8cfeadb5a7cfb77
Derrick Owusu-Asare <aderrick649@gmail.com>
2025-12-07 00:19:07 +0000

    styling

---
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -0,0 +1,11 @@
+function UserProfile() {
+  return (
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto, my-20 rounded-lg shadow-lg">
+      <img src="https://via.placeholder.com/150" alt="User" />
+      <h1>John Doe</h1>
+      <p>Developer at Example Co. Loves to write code and explore new technologies.</p>
+    </div>
+  );
+}
+
+export default UserProfile;
